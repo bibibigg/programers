9586d9689b506b7bc5e0376c90ad30be68afc3c2
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-10 20:57:23 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
-        if (angle < 90) {
+        if (angle > 0 && angle < 90) {
             answer = 1;
         } else if (angle == 90) {
             answer = 2;
