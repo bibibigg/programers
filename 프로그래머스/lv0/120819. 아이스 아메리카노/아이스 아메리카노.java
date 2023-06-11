6595a14da433706de96221500da30a02154b1ba8
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-12 01:27:51 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        if (money < 5500) {
+            answer[0] = 0;
+            answer[1] = money;
+        } else if (money % 5500 == 0) {
+            answer[0] = money / 5500;
+            answer[1] = 0;
+        } else {
+            answer[0] = money / 5500;
+            answer[1] = money - (answer[0] * 5500);
+        }
+            
+        return answer;
+    }
+}
