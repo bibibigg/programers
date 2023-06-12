e7ec5186a83c87cfd489a1ee9f8a451b9531bc82
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-12 20:30:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,10 +1,13 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int [(n + 1) / 2];
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) { // 조건문으로 인해서 짝수는 들어가지 않음
-                answer[i/2] = i; //i/2를 해줌으로써 홀수인 i가 차례대로 배열에 들어감
-            }
+        int[] answer;
+        if (n / 2 == 0) {
+        answer = new int[n / 2];
+        } else {
+            answer = new int[n / 2 + 1];
+        }
+        for (int i = 0; i < answer.length; i++) {
+            
         }
         return answer;
     }
