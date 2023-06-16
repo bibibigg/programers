0d262cb009004f3682096a53e1001f6b29a10bd7
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-16 17:17:33 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int pizza = 6; //피자 한판은 6조각
+        int pizza = 6; 
         int count = 1;
-        if (n == pizza) { //사람인원 / 6조각 == 1판 
-            answer = 1; // 1판 리턴
+        if (n == pizza) { 
+            answer = 1; 
         } else {
-           while((pizza % n) != 0) { // n 나누기 피자조각의 나머지가 0이 아닐시
-               pizza += 6; // 피자1판 추가
+           while((pizza % n) != 0) { 
+               pizza += 6; 
                count++;
            } 
         answer = count; 
