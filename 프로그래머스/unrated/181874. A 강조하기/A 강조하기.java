384c500b63d764ce29d36f3263b16c1099225900
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-22 20:59:38 +0900

    [unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,18 +1,10 @@
+
 class Solution {
+
     public String solution(String myString) {
-        String answer = "";
-            for (int i = 0; i < myString.length(); i++) {
-                char c = myString.charAt(i);
-                if (c == 'a') {
-                    answer += Character.toUpperCase(c);
-                    // answer += String.valueOf(c);
-                } else if (c != 'A'){
-                   answer += Character.toLowerCase(c);
-                       // answer += String.valueOf(c);
-                } else if ( c == 'A') {
-                    answer += c;
-                }
-            }
-        return answer;
-        }
+        myString = myString.toLowerCase();
+        myString = myString.replace('a', 'A');
+
+        return myString;
     }
+}
