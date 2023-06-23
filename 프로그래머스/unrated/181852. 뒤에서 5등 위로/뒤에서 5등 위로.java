84e87e2623ca1a4f15b5baae9215b6f2549c3c46
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-23 18:27:40 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.34 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length - 5]; // 가장 작은 5개의 수를 제외한 나머지 배열 개수
+        Arrays.sort(num_list); //가장 작은 5개의 수를 제외한 오름차순을 구하기 위해 정렬
+        answer = Arrays.copyOfRange(num_list, 5, num_list.length); // 가장작은 5개의 수 이후의 수를 복사
+        return answer;
+    }
+}
