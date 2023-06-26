8d04fdb32208add3be2af3e5e5c8a7c7c9c1b788
bibibigg <121605682+bibibigg@users.noreply.github.com>
2023-06-26 19:25:19 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -4,13 +4,13 @@ class Solution {
         int count = 0;
         
         for (int i = 0; i < arr.length; i++) {
-            count += arr[i];
+            count += arr[i];//반복문을 통하여 배열길이 계산, 배열길이는 arr배열의 값을 더한 값이 된다.
         }
         
         answer = new int[count];//길이설정 arr배열값을 더한값으로 배열길이 설정
         int index = 0;
             
-        for (int j = 0; j < arr.length; j++) {
+        for (int j = 0; j < arr.length; j++) { // 이중 반복문으로 arr[j]의 값을 arr[j]의 수만큼 반복 
             for (int a = 0; a < arr[j]; a++) {
                 answer[index++] += arr[j];
             }
