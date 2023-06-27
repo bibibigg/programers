a3f451d467eb7fec5d66dd26d81b3e247a8fc9b3
병진 <121605682+bibibigg@users.noreply.github.com>
2023-06-28 00:50:43 +0900

    [level 0] Title: 가까운 수, Time: 0.33 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -5,7 +5,9 @@ class Solution {
         int[] resultarr = new int[array.length];
         //n만큼 뺀 수를 배열에 저장 후배열에서 가장 작은수의 인덱스 값을 구해서 array배열에 적용하기 위해 배열 생성
         
-        Arrays.sort(array);
+        Arrays.sort(array); // 정렬을 한 이유는 arr배열에 10과 15가 있고 n이 5일시 이중 가까운 수는
+                            // 10 과 15 둘다 편차가 5로 같다
+                            // 문제 조건에서 가장 가까운 수가 여러개일 경우 더 작은수를 return하라 하였기에 정렬!!
         
         for(int i = 0; i < array.length; i++) {
             if (n > array[i]) { //조건문을 사용하여 음수가 나오지 않게 조정
@@ -20,9 +22,10 @@ class Solution {
         int minValue = resultarr[0]; //가장 작은수의 초기값을 resultarr[0]의 값으로 줌
         
         for (int j = 1; j < resultarr.length; j++) {
-            if (resultarr[j] < minValue) {
-                minValue = resultarr[j];
-                minIndex = j;
+            if (resultarr[j] < minValue) { // 초기값과 resultarr[j]을 비교
+                minValue = resultarr[j]; // 작을경우 minValue에 resultarr[j]을 저장하여 가장작은수 최신화
+                minIndex = j; // 가장 작은수의 인덱스 값은 j
+                              // 가장 가까운 수가 여러개일 경우 조건문에 맞지 않기에 여러개중 제일 작은수가 리턴
             }
         }
         
