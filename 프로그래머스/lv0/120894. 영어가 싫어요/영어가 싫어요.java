482e5f1592e0c8ce6158c4cdf19fc1b14168aa85
병진 <121605682+bibibigg@users.noreply.github.com>
2023-06-28 23:17:54 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        //replace함수를 사용하여 문자를 숫자문자로 바꾼후 Integer.parseInt로 정수로 변환
+        numbers = numbers.replace("zero","0");
+        numbers = numbers.replace("one","1");
+        numbers = numbers.replace("two","2");
+        numbers = numbers.replace("three","3");
+        numbers = numbers.replace("four","4");
+        numbers = numbers.replace("five","5");
+        numbers = numbers.replace("six","6");
+        numbers = numbers.replace("seven","7");
+        numbers = numbers.replace("eight","8");
+        numbers = numbers.replace("nine","9");
+        
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
+}
