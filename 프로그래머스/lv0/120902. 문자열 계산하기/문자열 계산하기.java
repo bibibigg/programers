75d999703fcd4823e3ce258c97cb4b73945acd2a
병진 <121605682+bibibigg@users.noreply.github.com>
2023-07-04 18:46:36 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.08 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String[] strarr = my_string.split(" ");
-        answer = Integer.parseInt(strarr[0]);
+        String[] strarr = my_string.split(" "); //공백을 기준으로 배열자르기
+        answer = Integer.parseInt(strarr[0]); // answer에는 배열 처음값을 설정
         for (int i = 0; i < strarr.length; i++) {
-            if (strarr[i].equals("+")) {
-                answer += Integer.parseInt(strarr[i+1]);
-            } else if (strarr[i].equals("-")) {
-                answer -= Integer.parseInt(strarr[i+1]);
+            if (strarr[i].equals("+")) { // strarr[i]가 "+" 문자일시
+                answer += Integer.parseInt(strarr[i+1]); // answer + "+"가나온 다음 배열값을 더해준다
+            } else if (strarr[i].equals("-")) { // strarr[i]가 "-" 문자일시
+                answer -= Integer.parseInt(strarr[i+1]); // answer - "-"가나온 다음 배열값을 빼준다
             }
         }
         return answer;
