d86ff761796ff16e5e89422ca339e24327ebbf9e
병진 <121605682+bibibigg@users.noreply.github.com>
2023-07-13 17:52:02 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -6,9 +6,7 @@ class Solution {
         
         //왼쪽 오른쪽 이동은 행
         //위 아래 이동은 열
-        //board는 최대좌표 11,11에서 x좌표는 0을 포함하여 
-                    // if (Math.abs(answer[0]) >= maxX || Math.abs(answer[1]) >= maxY) {
-            //     return answer;
+        //board는 최대좌표 11,11에서 x좌표는 0을 포함하여 좌우로 5까지만 이동가능 y도 마찬가지
         for (int i = 0; i < keyinput.length; i++) {
             switch (keyinput[i]) {
                 case "left" :
@@ -24,9 +22,9 @@ class Solution {
                     answer[1] += 1;
                     break;
                     }
-                    if (answer[0] > maxX) {
+                    if (answer[0] > maxX) { // answer가 maxX를 초과할시 -1을 하여 값이 증가하지 않게 함
             answer[0] -= 1;
-        } else if (Math.abs(answer[0]) > maxX) {
+        } else if (Math.abs(answer[0]) > maxX) { // answer가 -일시 +를 해주어서 값이 증가하지 않게함
             answer[0] += 1;
         } else if (answer[1] > maxY) {
             answer[1] -= 1;     
