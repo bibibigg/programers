33c9d23287ca335010f5675faf7f2283fa49d678
병진 <121605682+bibibigg@users.noreply.github.com>
2023-07-13 09:39:58 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -4,18 +4,18 @@ class Solution {
         // 열값만 추출하여 x좌표차 계산
         // 행값만 추출하여 y좌표차 계산
         // 두 값을 곱해줌
-                    int x1 = dots[0][0];
+                    int x1 = dots[0][0]; //dots배열의 처음 x값
                     int x2 = 0;
         int resultX = 0;
-        int y1 = dots[0][1];
+        int y1 = dots[0][1];//dots배열의 처음 y값
         int y2 = 0;
         int resultY = 0;
         for (int i = 1; i < dots.length; i++) {
 
-            if (x1 != dots[i][0]) {
+            if (x1 != dots[i][0]) { //x값이 다를시 그 값을 x2변수에 저장
                 x2 = dots[i][0];
             }
-            resultX = Math.abs(x1 - x2);
+            resultX = Math.abs(x1 - x2); // 두 x값의 차이를 절대값으로 계산
         }
         
          for (int j = 1; j < dots[0].length; j++) {
