be2ca4f62ee8800d45fa06e577c073dcf0911c3c
병진 <121605682+bibibigg@users.noreply.github.com>
2023-07-27 21:40:47 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answerList = new ArrayList<>();
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                answerList.add(todo_list[i]);
+            }
+        }
+        String[] answer = answerList.toArray(new String[0]);
+        return answer;
+    }
+}
