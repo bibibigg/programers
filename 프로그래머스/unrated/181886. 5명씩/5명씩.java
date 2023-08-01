f37efc53d8a32eebb60ab99cabe0f976095ebe7d
병진 <121605682+bibibigg@users.noreply.github.com>
2023-08-01 19:21:36 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 class Solution {
     public String[] solution(String[] names) {
         String[] answer;
         ArrayList<String> resultList = new ArrayList<>();
         
-        //answer[0] = names[0];
         for (int i = 0; i < names.length; i++) {
             if (i % 5 == 0) {
                 resultList.add(names[i]);
